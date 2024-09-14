a87103c4917cf2b17899d8c3011a62528eaaf6ec
Carlos Eduardo Dias Batista <carloseduardodiasbatista@gmail.com>
2024-09-14 11:00:17 -0300

    init

--- public/src/components/header/index.tsx
+++ public/src/components/header/index.tsx
@@ -4,13 +4,7 @@ const Header = () => (
   <header className="bg-white shadow-md">
     <div className="flex flex-row py-5 px-4">
       <a href="/" className="flex flex-row items-center gap-x-3">
-        <img
-          src="../../assets/digipetro-logo.png"
-          alt="Digipetro Logo"
-          height="32"
-          width="32"
-        />
-        <h1 class="text-xl text-green-900">DigiPay</h1>
+        <h1 class="text-xl text-green-900">Light Micro SaaS</h1>
       </a>
     </div>
   </header>
